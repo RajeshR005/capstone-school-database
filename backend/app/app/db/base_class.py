031f514abf6509b292cf6b4e5a4eaf00e02efe2b
Rajesh <rajeshr30072002@gmail.com>
2025-08-07 00:48:32 +0530

    Connect to Railway DB and prepare for deployment

--- backend/app/app/db/base_class.py
+++ backend/app/app/db/base_class.py
@@ -1,14 +1,18 @@
 from typing import Any
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 from sqlalchemy.orm import declarative_base
 
+load_dotenv()
 Base=declarative_base()
 
 
-db_url="mysql+pymysql://root:2741@localhost:3307/capstone_school_db"
+# db_url="mysql+pymysql://root:2741@localhost:3307/capstone_school_db"
+db_url=os.getenv("DATABASE_URL")
 
 engine=create_engine(db_url)
 
